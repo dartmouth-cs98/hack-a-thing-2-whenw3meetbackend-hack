6fc1775a42851ec19f748d17ffce0c73a0656e9e
giga-byt <bill.y.tang.20@dartmouth.edu>
2020-01-27 13:13:10 -0500

    successfully created and retrieved hashed rooms

--- server/server.js
+++ server/server.js
@@ -30,50 +30,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// this is our get method
-// this method fetches all available data in our database
-router.get('/getData', (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post('/updateData', (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, (err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete('/deleteData', (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, (err) => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post('/putData', (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: 'INVALID INPUTS',
-    });
+let hashCode = function(string) {
+  var hash = 0, i, chr;
+  if (string.length === 0) return hash;
+  for (i = 0; i < string.length; i++) {
+    chr   = string.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
   }
-  data.message = message;
-  data.id = id;
+  return hash;
+};
+
+router.get('/', (req,res) => {
+  return res.json({success:true, data: "Hello!"})
+})
+
+router.get('/room', (req, res) => {
+  Data.find({id:req.query.roomId}, (err, data) => {
+    if (err) return res.json({ success: false, error: "Room does not exist"});
+    return res.json({success: true, data: data});
+  });
+});
+
+router.post('/room', (req, res) => {
+  let data = new Data();
+  let roomTitle = req.query.title
+  let newRoomId = hashCode(roomTitle);
+
+  const param = { newRoomId, roomTitle };
+
+  data.message = param.roomTitle;
+  data.id = param.newRoomId;
   data.save((err) => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
